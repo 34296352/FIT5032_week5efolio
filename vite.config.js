0d886f48c953ff60929dc9a92ba00292ec767b6b
lijiawei <jlii0456@student.monash.edu>
2025-10-27 05:36:10 +1100

    fix(vite): add base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FIT5032_week5efolio/',
   plugins: [
     vue(),
     vueDevTools(),
